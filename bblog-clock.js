aecfb4c0841a2b61e1e6d4edb3a522ef220da8e1
Daniel Peukert <dan.peukert@gmail.com>
2014-05-12 18:33:00 +0200

    Bacground added + some minor fixes

--- bblog-clock.js
+++ bblog-clock.js
@@ -9,31 +9,23 @@
 // initialize your plugin
 BBLog.handle("add.plugin", {
     id : "bblog-clock",
-
+    
     name : "BetterBattlelog Clock",
-
-
-    translations : {
-    },
-
-    configFlags : [
-    ],
 
     init : function(instance){
       if($('#bblog-clock').length === 0)
         {
-          $('body').append('<div style="position: fixed; display: block; bottom:-4px;z-index:9999" id="bblog-clock"></div>');
+          $('body').append('<div style="position: fixed; display: block; bottom: 0px; padding: 0 4px; z-index:9999; background-color: rgba(0, 0, 0, 0.5)" id="bblog-clock"><h5 style="margin:0"></h5></div>');
         }
       setInterval(instance.setActualTime, 1000);
     },
   
     setActualTime : function(){
         var actualDate = new Date().toLocaleTimeString();
-        var innerDiv = '<h5>'+actualDate+'</h5>';
         
         if($('#bblog-clock').length != 0)
         {
-          $('#bblog-clock').html(innerDiv);
+          $('#bblog-clock h5').html(actualDate);
         }
     }
 });
