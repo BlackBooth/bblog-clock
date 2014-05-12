2c87874c92ee59ed894354154b0a535e90a2d2b5
BlackBooth <patrick.mueller.fn@gmail.com>
2014-05-12 18:51:25 +0200

    Added improvements from dapil
    Background added + minor style fixed

--- bblog-clock.js
+++ bblog-clock.js
@@ -1,39 +1,33 @@
 /**
 * BetterBattlelog Clock - Shows a clock in the bottom left corner
+* 
+* Thanks to dapil for the improvements: https://github.com/dapil/
 *
-* @author BlackBooth
-* @version 1.0
+* @author BlackBooth, dapil
+* @version 1.1
 * @url https://github.com/BlackBooth/bblog-clock
 */
 
 // initialize your plugin
 BBLog.handle("add.plugin", {
     id : "bblog-clock",
-
+    
     name : "BetterBattlelog Clock",
-
-
-    translations : {
-    },
-
-    configFlags : [
-    ],
 
     init : function(instance){
       if($('#bblog-clock').length === 0)
         {
-          $('body').append('<div style="position: fixed; display: block; bottom:-4px;z-index:9999" id="bblog-clock"></div>');
+          $('body').append('<div style="position: fixed; display: block; bottom: 0px; padding: 0 4px; z-index:9999; background-color: rgba(0, 0, 0, 0.5)" id="bblog-clock"><h5 style="margin:0"></h5></div>');
         }
       setInterval(instance.setActualTime, 1000);
     },
   
     setActualTime : function(){
         var actualDate = new Date().toLocaleTimeString();
-        var innerDiv = '<h5>'+actualDate+'</h5>';
         
         if($('#bblog-clock').length != 0)
         {
-          $('#bblog-clock').html(innerDiv);
+          $('#bblog-clock h5').html(actualDate);
         }
     }
 });
